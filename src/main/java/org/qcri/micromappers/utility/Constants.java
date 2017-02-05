95d501976da12f9922777480351b6c643c7f4160
jikimlucas <jikimlucas@me.com>
2017-02-05 11:46:42 +0300

    for stopword reading is getting error. it has to use resource getinputstream

--- src/main/java/org/qcri/micromappers/utility/Constants.java
+++ src/main/java/org/qcri/micromappers/utility/Constants.java
@@ -1,9 +1,12 @@
 package org.qcri.micromappers.utility;
 
+import org.apache.log4j.Logger;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,6 +17,8 @@ import java.util.stream.Collectors;
 
 public class Constants
 {
+  private static Logger logger = Logger.getLogger(Constants.class);
+
   public static long ONE_HOUR_IN_MILLISECS = 3600000L;
   
   public static final String GDELT_PROCESSED_EXTENTION = ".processed";
@@ -30,14 +35,16 @@ public class Constants
   public static List<String> STOP_WORDS;
 
   public static void populateStopWords() throws Exception {
-    String filePath = new ClassPathResource("stopwords_minimal.txt").getFile().getAbsolutePath();
+    InputStream is = new ClassPathResource("stopwords_minimal.txt").getInputStream();
     List<String> stopWords= new ArrayList<>();
-    try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath), Charset.forName("UTF-8")))
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(is)))
     {
       stopWords = Arrays.asList(br.lines().collect(Collectors.toList()).get(0).split(","));
 
     } catch (IOException e) {
-      e.printStackTrace();
+      //e.printStackTrace();
+      logger.error("populateStopWords : " + e);
     }
 
     STOP_WORDS = stopWords;
